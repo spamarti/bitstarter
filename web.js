b675a44d301a534b90e4ef2825fd2080fae12c0e
spamarti <siva.pamarti@gmail.com>
2013-09-06 23:15:58 +0000

    latest

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Yog Ratova Bhog Ratova');
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  response.send(data);
+});
+
+ // response.send('Yog Ratova Bhog Ratova');
 });
 
 var port = process.env.PORT || 5000;
